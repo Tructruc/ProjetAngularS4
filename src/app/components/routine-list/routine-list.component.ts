ad9403d570f36f2b99666e7d08a7db360579025f
Tructruc <emilien.fieu@etu.univ-tlse2.fr>
2024-02-09 23:50:12 +0100

    routine filter

--- src/app/components/routine-list/routine-list.component.ts
+++ src/app/components/routine-list/routine-list.component.ts
@@ -18,4 +18,25 @@ export class RoutineListComponent implements OnInit {
   ngOnInit(): void {
     this.routines = this.routineService.getAllRoutines()
   }
+
+  closeDropdown(event: MouseEvent, dropdownId: string): void {
+    event.stopPropagation(); // Stop propagation to keep the menu from reopening
+    const dropdownElement = document.getElementById(dropdownId);
+
+    const dropdownButton = dropdownElement?.querySelector('.dropdown-toggle');
+
+    // remove the show class from the dropdown
+    dropdownButton?.classList.remove("show");
+    // remove the inline style from the dropdown
+    dropdownButton?.removeAttribute("style");
+    // put aria-expanded to false
+    dropdownButton?.setAttribute("aria-expanded", "false");
+
+
+    // remove the show class from the dropdown menu
+    const dropdownMenu = dropdownElement?.querySelector('.dropdown-menu');
+    dropdownMenu?.classList.remove("show");
+  }
+
+  protected readonly console = console;
 }
